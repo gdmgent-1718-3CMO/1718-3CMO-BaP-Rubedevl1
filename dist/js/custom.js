75f6f53d385eab9e218ff37a2ed9bd09977e0c4a
rubedevl1 <rubedevl1@student.arteveldehs.be>
2018-05-14 23:23:24 +0200

    test

---
+++ dist/js/custom.js
@@ -0,0 +1,47 @@
+    $(document).ready(function () {
+
+        //Draw Menu 
+        $('#navbarsExampleDefault').html(
+        '<ul class="navbar-nav ml-auto green">'+
+        '<li class="nav-item active">'+
+        '  <a class="nav-link" href="index.html">Home</a>'+//1
+        '</li>'+
+        '<li class="nav-item active">'+
+        '  <a class="nav-link" href="missie.html">Mission</a>'+//2
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link" href="logo.html">Logo</a>'+//3
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link " href="kleurenpallet.html">Kleurenpallet</a>'+//4
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link " href="typografic.html">Typografic</a>'+//5
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link " href="krachtlijnen.html">Krachtlijnen</a>'+//6
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link " href="paper.html">Paper</a>'+//7
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link " href="sport.html">Sport</a>'+//8
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link " href="evenement.html">Event</a>'+//9
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link " href="website.html">Website</a>'+//10
+        '</li>'+
+        '<li class="nav-item">'+
+        '  <a class="nav-link " href="presentatie.html">Presentatie</a>'+//11
+        '</li>'+
+         '<li class="nav-item">'+
+        '  <a class="nav-link " href="wagenpk.html">Wagenpk</a>'+//12
+        '</li>'+
+        '</ul>');
+
+
+        //Draw Footer 
+
+    });
